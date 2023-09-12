ac3a666589fe9aacab4fb1679184d0c18f649636
Balaji Shetty Pachai <balaji.pachai08@gmail.com>
2023-09-12 06:10:24 +0530

    wip: shortest path in unweighted graphs

--- ds-algo-made-easy/chapter-9-graph-algorithms/graphs/graphs.go
+++ ds-algo-made-easy/chapter-9-graph-algorithms/graphs/graphs.go
@@ -10,22 +10,39 @@ const (
 	colorRed     = "\033[31m"
 )
 
+// The Graph type represents a graph with a given number of vertices and an adjacency list.
+// @property {int} vertices - The "vertices" property represents the number of vertices (or nodes) in
+// the graph. It indicates the total number of nodes present in the graph.
+// @property adjList - The `adjList` property is a map that represents the adjacency list of the graph.
+// It maps each vertex to a list of its adjacent vertices.
 type Graph struct {
 	vertices int
 	adjList  map[int][]int
 }
 
+// The `NewGraph` function is a method of the `Graph` struct. It is used to create a new instance of
+// the `Graph` struct with the specified number of vertices.
 func (g *Graph) NewGraph(numVertices int) *Graph {
 	g.vertices = numVertices
 	g.adjList = make(map[int][]int)
 	return g
 }
 
+// The `AddEdge` function is a method of the `Graph` struct. It is used to add an undirected edge
+// between two vertices in the graph.
 func (g *Graph) AddEdge(v1, v2 int) {
 	g.adjList[v1] = append(g.adjList[v1], v2)
 	g.adjList[v2] = append(g.adjList[v2], v1)
 }
 
+// The `AddDirectedEdge` function is a method of the `Graph` struct. It is used to add a directed edge
+// from vertex `v1` to vertex `v2` in the graph.
+func (g *Graph) AddDirectedEdge(v1, v2 int) {
+	g.adjList[v1] = append(g.adjList[v1], v2)
+}
+
+// The `PrintAdjList` function is a method of the `Graph` struct. It is used to print the adjacency
+// list of the graph.
 func (g *Graph) PrintAdjList() {
 	fmt.Print(colorMagenta)
 	for vertex, neighbours := range g.adjList {
@@ -59,6 +76,8 @@ func (g *Graph) DFS(v int) {
 	DFSHelper(g, v, visited)
 }
 
+// The `BFS` function is a method of the `Graph` struct. It performs a Breadth First Search traversal
+// starting from a given vertex `v`.
 func (g *Graph) BFS(v int) {
 	visited = make([]bool, g.vertices)
 	queue := make([]int, 0)
@@ -77,4 +96,72 @@ func (g *Graph) BFS(v int) {
 		queue = queue[1:]
 	}
 	fmt.Print(colorReset)
+}
+
+var stack = make([]int, 0)
+
+// The function performs a depth-first search on a graph and appends the visited vertices to a stack in topological order.
+func TopologicalSortHelper(g *Graph, v int, visited []bool) {
+	visited[v] = true
+	neighbours := g.adjList[v]
+
+	for _, neighbour := range neighbours {
+		if !visited[neighbour] {
+			TopologicalSortHelper(g, neighbour, visited)
+		}
+	}
+	stack = append(stack, v)
+}
+
+// The function performs a topological sort on the graph.
+func (g *Graph) TopologicalSort() {
+	visited = make([]bool, g.vertices)
+
+	for i := 0; i < g.vertices; i++ {
+		if !visited[i] {
+			TopologicalSortHelper(g, i, visited)
+		}
+	}
+
+	fmt.Println(colorMagenta)
+	// Using only array as a stack
+	for i := len(stack) - 1; i >= 0; i-- {
+		fmt.Printf("\t%d", stack[i])
+	}
+	fmt.Println(colorReset)
+}
+
+// The `UnweightedShortestPath` function calculates the shortest path from a given source vertex `s` to
+// all other vertices in the graph using the unweighted shortest path algorithm.
+func (g *Graph) UnweightedShortestPath(s int) {
+	queue := make([]int, 0)
+	distance := make([]int, g.vertices)
+	path := make([]int, g.vertices)
+
+	queue = append(queue, s) // EnQueue
+	for i := 0; i < g.vertices; i++ {
+		distance[i] = -1
+	}
+	distance[s] = 0
+	for len(queue) > 0 {
+		v := queue[0]
+		neighbours := g.adjList[v]
+		queue = queue[1:] // DeQueue
+		for _, neighbour := range neighbours {
+			if distance[neighbour] == -1 {
+				distance[neighbour] = distance[v] + 1
+				path[neighbour] = v
+				queue = append(queue, neighbour) // EnQueue
+			}
+		}
+	}
+	fmt.Println(colorMagenta, "\tDistance from vertex ", s, " to:")
+	for i, d := range distance {
+		fmt.Printf("\t\tVertex = %d\tDistance = %d\n", i, d)
+	}
+	// fmt.Println("\tPath from vertex s = ", s)
+	// for _, p := range distance {
+	// 	// fmt.Printf("\t\tVertex = %d\tPath = %d\n", i, p)
+	// 	fmt.Printf("\t\tVertex name through which we get shortest distance: %d\n", p)
+	// }
 }
--- ds-algo-made-easy/chapter-9-graph-algorithms/main/index.go
+++ ds-algo-made-easy/chapter-9-graph-algorithms/main/index.go
@@ -21,25 +21,74 @@ func printDottedLine() {
 
 func GraphBasics() {
 	fmt.Println(colorYellow + "\tCreating a graph" + colorReset)
-	g := &graphs.Graph{}
-	g.NewGraph(5)
 
-	g.AddEdge(0, 1)
-	g.AddEdge(0, 2)
-	g.AddEdge(0, 3)
-	g.AddEdge(2, 3)
-	g.AddEdge(2, 4)
-
-	fmt.Println(colorYellow + "\tPrinting the Graph's Adjaceny List" + colorReset)
-	g.PrintAdjList()
-	fmt.Println(colorYellow + "\tDFS Traversal of the graph starting at vertex 2 is:::" + colorReset)
-	g.DFS(2)
+	// DFS
+	fmt.Println(colorCyan + "\tDEPTH FIRST SEARCH" + colorReset)
+	g1 := &graphs.Graph{}
+	g1.NewGraph(5)
+	g1.AddEdge(0, 1)
+	g1.AddEdge(0, 2)
+	g1.AddEdge(0, 3)
+	g1.AddEdge(2, 3)
+	g1.AddEdge(2, 4)
+	fmt.Println(colorYellow + "\tPrinting the Graph's (G1) Adjaceny List" + colorReset)
+	g1.PrintAdjList()
+	fmt.Println(colorYellow + "\tDFS Traversal of the graph (G1) starting at vertex 2 is:::" + colorReset)
+	g1.DFS(2)
 	fmt.Println()
 	printDottedLine()
 
-	fmt.Println(colorYellow + "\tBFS Traversal of the graph starting at vertex 1 is:::" + colorReset)
-	g.BFS(1)
+	// BFS
+	fmt.Println(colorCyan + "\tBREADTH FIRST SEARCH" + colorReset)
+	g2 := &graphs.Graph{}
+	g2.NewGraph(4)
+	g2.AddEdge(0, 1)
+	g2.AddEdge(0, 2)
+	g2.AddEdge(1, 2)
+	g2.AddEdge(2, 0)
+	g2.AddEdge(2, 3)
+	g2.AddEdge(3, 3)
+	fmt.Println(colorYellow + "\tPrinting the Graph's (G2) Adjaceny List" + colorReset)
+	g2.PrintAdjList()
+	fmt.Println(colorYellow + "\tBFS Traversal of the graph (G2) starting at vertex 1 is:::" + colorReset)
+	g2.BFS(2)
 	fmt.Println()
+	printDottedLine()
+
+	// Topological Sorting
+	fmt.Println(colorCyan + "\tTOPOLOGICAL SORTING" + colorReset)
+	dg := &graphs.Graph{}
+	dg.NewGraph(6)
+	dg.AddDirectedEdge(5, 2)
+	dg.AddDirectedEdge(5, 0)
+	dg.AddDirectedEdge(4, 0)
+	dg.AddDirectedEdge(4, 1)
+	dg.AddDirectedEdge(2, 3)
+	dg.AddDirectedEdge(3, 1)
+	fmt.Println(colorYellow + "\tPrinting the Graph's (DG-Directed Graph) Adjaceny List" + colorReset)
+	dg.PrintAdjList()
+	fmt.Println(colorYellow + "\tTopological Sort of the graph (DG):::" + colorReset)
+	dg.TopologicalSort()
+	fmt.Println()
+	printDottedLine()
+
+	// Shortest Path Algorithm
+	fmt.Println(colorCyan + "\tSHORTEST PATH ALGORITHMS" + colorReset)
+	g3 := &graphs.Graph{}
+	g3.NewGraph(7)
+	g3.AddDirectedEdge(0, 1)
+	g3.AddDirectedEdge(0, 3)
+	g3.AddDirectedEdge(1, 3)
+	g3.AddDirectedEdge(1, 4)
+	g3.AddDirectedEdge(2, 0)
+	g3.AddDirectedEdge(2, 5)
+	g3.AddDirectedEdge(3, 5)
+	g3.AddDirectedEdge(3, 6)
+	g3.AddDirectedEdge(4, 6)
+	fmt.Println(colorYellow + "\tPrinting the G3's Adjaceny List" + colorReset)
+	g3.PrintAdjList()
+	fmt.Println(colorYellow + "\tUnweightedShortestPath (G3) starting from vertex 2 :::" + colorReset)
+	g3.UnweightedShortestPath(2)
 	printDottedLine()
 }
 
